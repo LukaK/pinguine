94a3dc42103373effd047003fc1cc14a68c7561b
Luka Krapic <luka.krapic@gmail.com>
2023-03-13 09:01:22 +0100

    Initialize repository

---
+++ base-uefi.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# partition and format the disc
+
+# timezone setup
+ln -sf /usr/share/zoneinfo/Europe/Zagreb /etc/localtime
+hwclock --systohc
+
+# locale setup (English and Croatia)
+sed -i '178s/.//' /etc/locale.gen
+sed -i '273s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+# echo "KEYMAP=de_CH-latin1" >> /etc/vconsole.conf
+
+# hostname setup
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+
+# setting root password
+echo root:password | chpasswd
+
+# You can remove the tlp package if you are installing on a desktop or vm
+pacman -S efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+
+# graphic driver setup
+# pacman -S --noconfirm xf86-video-amdgpu
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+# setup boot manager
+# grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+# grub-mkconfig -o /boot/grub/grub.cfg
+
+# enable services
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+# systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable libvirtd
+systemctl enable firewalld
+systemctl enable acpid
+
+useradd -m luka
+echo luka:password | chpasswd
+usermod -aG libvirt,wheel luka
+
+echo "luka ALL=(ALL) ALL" >> /etc/sudoers.d/luka
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
---
+++ i3.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# date and time setup
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+# firewall
+sudo firewall-cmd --set-default-zone=home
+sudo firewall-cmd --add-service ftp --permanent
+sudo firewall-cmd --remove-service ssh --permanent
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --add-service libvirt --zone=libvirt --permanent
+sudo firewall-cmd --reload
+
+
+# packages
+sudo paru -S --noconfirm --skipreview xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings simplescreenrecorder obs-studio papirus-icon-theme vlc viber thunderbird keepassxc xclip google-chrome dropbox libreoffice flameshot zoom newsboat zathura zathura-djvu zathura-ps zathura-cb zathura-pdf-mupdf ttf-dejavu ttf-liberation noto-fonts nitrogen picom lxappearance pcmanfm materia-gtk-theme archlinux-wallpaper stow alacritty zsh zsh-completions cups-pdf transmission-gtk transmission-cli python-neovim xsel wl-clipboard neovim fzf htop pyenv jenv zsh-syntax-highlighting nodejs npm wget ripgrep fd slack-desktop git-remote-codecommit dex i3 xfce4-terminal ttf-font-awesome ttf-ubuntu-font-family thunar thunar-volman gvfs arandr rofi ttf-droid pacman-contrib i3lock xorg-fonts-misc siji-git ttf-unifont awesome-terminal-fonts ttf-sourcecodepro-nerd
+
+
+# enable display manager
+sudo systemctl enable lightdm
+
+# oh-my-zsh with powerlevel 10k
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# setup virtualenvwrapper with pyenv
+git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git $(pyenv root)/plugins/pyenv-virtualenvwrapper
+
+# avahi setup for cups and so on
+
+# cusomizing themes with lxappearance, rofi-theme-selector
+
+# customize greeter
+
+# TODO: Download dotfiles and set it up
+# TODO: Download vim and set it up
+# TODO: Download dropbox files and link them
+# TODO: Zsh syntax highlighting not working
+
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+sleep 5
+reboot
