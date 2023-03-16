e4740776627590f16d6f6410d93ed385863a4d51
Luka Krapic <luka.krapic@gmail.com>
2023-03-16 22:18:30 +0100

    Fix seeding the bootloader uuids

--- base-uefi.sh
+++ base-uefi.sh
@@ -65,8 +65,8 @@ bootctl --path=/boot install
 echo "timeout 5" >> /boot/loader/loader.conf
 echo "default arch" >> /boot/loader/loader.conf
 cp ./pinguine/arch.conf /boot/loader/entries
-sed -i "s/uuiddevice/$(lsblk -n -o UUID ${DISK}2 | head -n 1)/" /boot/loader/entries/arch.conf
-sed -i "s/uuidroot/$(lsblk -n -o UUID ${DISK}2 | tail -n 1)/" /boot/loader/entries/arch.conf
+sed -i "s/uuiddevice/$(blkid --output value ${DISK}2 | head -n 1)/" /boot/loader/entries/arch.conf
+sed -i "s/uuidroot/$(lsblk -n -o UUID /dev/mapper/root | head -n 1)/" /boot/loader/entries/arch.conf
 cp /boot/loader/entries/arch.conf /boot/loader/entries/arch-fallback.conf
 sed -i "s/initramfs-linux.img/initramfs-linux-fallback.img/" /boot/loader/entries/arch-fallback.conf
 
