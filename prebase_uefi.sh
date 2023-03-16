6ea26bcadf6439d882fb688e8e05ce1e40dd328c
Luka Krapic <luka.krapic@gmail.com>
2023-03-16 21:24:26 +0100

    Fix typo and add output from the pre-base script

--- prebase_uefi.sh
+++ prebase_uefi.sh
@@ -4,15 +4,18 @@
 DISK="/dev/vda"
 
 # partition the disk
+echo "Partitioning the disk $DISK..."
 sgdisk -n 1:0:+300M -t 1:ef00 -n 2:301M $DISK
 
 # format the disks
+echo "Formatting the disk..."
 mkfs.fat -F32 "${DISK}1"
 cryptsetup --cipher aes-xts-plain64 --hash sha512 --use-random --verify-passphrase luksFormat "${DISK}2"
 cryptsetup luksOpen "${DISK}2" root
 mkfs.btrfs /dev/mapper/root
 
 # create subvolumes
+echo "Creating btrfs subvolumes..."
 mount /dev/mapper/root /mnt
 pushd /mnt
 btrfs subvolume create @
@@ -21,6 +24,7 @@ popd
 umount /mnt
 
 # mount subvolumes
+echo "Mounting partitions to /mnt..."
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@ /dev/mapper/root /mnt
 mkdir /mnt/{boot,home}
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@home /dev/mapper/root /mnt/home
@@ -28,4 +32,5 @@ mount "${DISK}1" /mnt/boot
 
 # NOTE: Change intel-ucode to amd-ucode for amd processor
 # base system
-pacstram /mnt base linux linux-firmware git vim intel-ucode
+echo "Pacstraping the system..."
+pacstrap /mnt base linux linux-firmware git vim intel-ucode
