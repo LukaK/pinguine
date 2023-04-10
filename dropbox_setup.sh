dacbbbc81a6ad312e3eae70c62ba30aec145f025
Luka Krapic <luka.krapic@gmail.com>
2023-04-10 15:06:45 +0200

    Add dropbox setup script

---
+++ dropbox_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+mkdir -p ~/Workspace
+ln -s ~/Dropbox/WorkSynced ~/Workspace/Sync
+ln -s ~/Dropbox/Archive ~/Archive
+ln -s ~/Dropbox/General_reference ~/General_reference
+ln -s ~/Dropbox/Project_reference ~/Project_reference
